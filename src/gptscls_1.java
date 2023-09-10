08078473d9269fa950a23e1befe3e978980bf372
DhinakaranP <sarathidhina@gmail.com>
2023-09-10 10:16:03 +0530

    add

---
+++ src/gptscls_1.java
@@ -0,0 +1,18 @@
+
+public class gptscls_1 {
+	
+	private void add() {	
+		
+		System.out.println("add");
+		
+	}
+
+		public static void main(String[] args) {	
+			
+		gptscls_1 obj=new gptscls_1();
+		
+		obj.add();
+		
+	}
+	
+}
